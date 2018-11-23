9b280286a919c9d6361d19a6bcbd09077c017b78
Apcloveras <38353341+LanyDa@users.noreply.github.com>
2018-11-23 19:06:45 +0800

    Create 过河卒_深搜_动归.cpp

---
+++ codevs/过河卒_深搜_动归.cpp
@@ -0,0 +1,81 @@
+//问题链接：http://codevs.cn/problem/1010/
+//两种解法
+//1,动态规划解法
+#include<iostream>
+using namespace std;
+int n,m;
+int x,y;
+int dp[20][20];
+int f[20][20];
+void init(){
+    if(x>=2) {
+        if(y-1>=0)
+            f[x - 2][y-1] = 1;
+        if(y+1<=m)
+            f[x - 2][y+1] = 1;
+    }
+    if(x+2 <= n){
+        if(y-1>=0)
+            f[x + 2][y-1] = 1;
+        if(y+1<=m)
+            f[x + 2][y+1] = 1;
+    }
+    if(y - 2 >= 0){
+        if(x - 1 >= 0)
+            f[x - 1][y-2] = 1;
+        if(x + 1 <= n)
+            f[x + 1][y-2] = 1;
+    }
+    if(y + 2 <= m) {
+        if (x - 1 >= 0)
+            f[x - 1][y + 2] = 1;
+        if (x + 1 <= n)
+            f[x + 1][y + 2] = 1;
+    }
+    f[x][y] = 1;
+    dp[0][0] =1;
+}
+int main(){
+    cin >> n >> m;
+    cin >> x >> y;
+    //动态规划
+    //定义状态： dp[i][j] 就是到i,j的路径条数==>最优子结构
+    //状态转移： dp[i][j] = dp[i][j-1] + dp[i-1][j] ()
+    //                   = 0  i,j 在马的控制范围内 
+    init();
+    for(int i = 0;i <= n;i++){
+        for(int j = 0;j <= m;j++)
+            cout << f[i][j] <<" ";
+        cout<< endl;
+    }
+    for(int i = 0;i <= m;i++){
+        if(f[0][i] == 1)
+            break;
+        dp[0][i] = 1;
+    }for(int i = 0;i <= n;i++){
+        if(f[i][0] == 1)
+            break;
+        dp[i][0] = 1;
+    }
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= m;j++){
+            if(f[i][j] == 1){
+                dp[i][j] = 0;
+                continue;
+            }
+            dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+    }
+    cout << "################"<<endl;
+    
+    for(int i = 0;i <= n;i++){
+        for(int j = 0;j <= m;j++)
+            cout <<dp[i][j] <<" ";
+        cout << endl;
+    }
+    cout << dp[n][m] << endl;
+    return 0;
+}
+
+
+//解法二：深搜
